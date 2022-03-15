1580a5fa345ca83ed95349d37696547abc6a2f1e
ArturDokhno <artur.dohno@icloud.com>
2022-03-15 19:27:06 +0500

    Запрос погоды и вывод данных пришедших с сервера в консоль.

---
+++ Sunny/Supporting files/Constans.swift
@@ -0,0 +1,11 @@
+//
+//  Constans.swift
+//  Sunny
+//
+//  Created by Артур Дохно on 15.03.2022.
+//  Copyright © 2022 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "6aae2b16f107d043e3de85ca9c6964ed"
